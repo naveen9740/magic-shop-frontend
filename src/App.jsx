2dfa518a6f7164d85270d37cba7162d1b168b977
NAVEEN KAMATH <naveen.kamath002@gmail.com>
2021-11-26 17:40:24 +0530

    feat: Added Navbar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
+import { Home } from "./pages/Home";
 
 const App = () => {
-  return <div>Hello</div>;
+  return <Home />;
 };
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Announcement } from "../components/Announcement";
+import { Navbar } from "../components/Navbar";
+
+export const Home = () => {
+  return (
+    <div>
+      <Announcement />
+      <Navbar />
+    </div>
+  );
+};
