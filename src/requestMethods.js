9bdfa136f5c4bd22c9ebdcec5dbc71c8122b7333
NAVEEN KAMATH <naveen.kamath002@gmail.com>
2021-12-02 02:21:02 +0530

    added backend server url

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = `http://localhost:5000/api/v1`;
+const baseUrl = `https://magic-shop-backend.herokuapp.com/api/v1`;
 const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxYTRjMjBhOTBlY2U1M2JmMmI5ZGRmZiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzODM4OTc5MSwiZXhwIjoxNjM4NDc2MTkxfQ.kGIh9P8gMaQcOwprXqJg8D185yZOc77WmXk0pi9v49E`;
 
 export const publicRequest = axios.create({
